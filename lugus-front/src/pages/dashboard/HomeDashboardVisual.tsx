78dc03eecd1f404b6175f434780b20cb4f135bbb
chusnaval <chusnaval@gmail.com>
2026-05-31 09:37:19 +0200

    Update HomeDashboardVisual.tsx

--- lugus-front/src/pages/dashboard/HomeDashboardVisual.tsx
+++ lugus-front/src/pages/dashboard/HomeDashboardVisual.tsx
@@ -18,7 +18,24 @@ export default function HomeDashboardVisual() {
         totalFilms: 0,
         recentFilms: 0,
         completeGroups: 0,
-        incompleteGroups: 0
+        incompleteGroups: 0,
+        vhs: 0,
+        dvd: 0,
+        bluray: 0,
+        uhd: 0,
+        digital: 0,
+        notOwned: 0,
+        generosPorCategoria: {
+            arteEntretenimiento: 0,
+            literaturaNarrativa: 0,
+            cienciaFiccion: 0,
+            accion: 0,
+            misterio: 0,
+            terror: 0,
+            conflicto: 0,
+            documental: 0
+        }
+
     })
     const [serieStats, setSerieStats] = useState<SeriesStats>({
         total: 0,
